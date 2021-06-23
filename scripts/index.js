fc16d8dffe109811705b0f0ea44a32a366ff39fe
Alexander Malanin <malaninaleck@yandex.ru>
2021-06-23 23:35:03 +0300

    2 коммит до итерации

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,31 @@ const profileElement = document.querySelector('.profile')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 const popupOpenButtonElement = document.querySelector('.profile__edit-button')
 
+const popupElementNameInput = popupElement.querySelector('.popup__item_name')
+const popupElementJobInput = popupElement.querySelector('.popup__item_job')
+
+const profileName = profileElement.querySelector('.profile__title')
+const profileJob = profileElement.querySelector('.profile__subtitle')
+
+const popupFormElement = popupElement.querySelector('.popup__form')
+const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button')
+
+
+const addTextProfile = function (evt) {
+    evt.preventDefault();
+    profileName.textContent = editName.value
+    profileJob.textContent = edinJob.value
+    closePopup()
+}
+
+
 const openPopup = function () {
-    popupElement.classList.add('popup_is_opened')
+    popupElement.classList.add('popup_is-opened')
 }
 const closePopup = function () {
-    popupElement.classList.remove('popup_is_opened')
+    popupElement.classList.remove('popup_is-opened')
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup)
 popupCloseButtonElement.addEventListener('click', closePopup)
+popupSubmitButtonElement.addEventListener('submit', addTextProfile);
