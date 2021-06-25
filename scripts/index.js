79c29dd099408a1248c1185f9c59b7968e55a6ab
Alexander Malanin <malaninaleck@yandex.ru>
2021-06-25 23:14:22 +0300

    изменения 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
+// Выборка DOM элементов, объявление переменных
 const popupElement = document.querySelector('.popup')
 const profileElement = document.querySelector('.profile')
-
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 const popupOpenButtonElement = document.querySelector('.profile__edit-button')
-
-const popupElementNameInput = popupElement.querySelector('.popup__item_name')
-const popupElementJobInput = popupElement.querySelector('.popup__item_job')
-
+const popupElementNameInput = popupElement.querySelector('.popup__item_type_name')
+const popupElementJobInput = popupElement.querySelector('.popup__item_type_job')
 const profileName = profileElement.querySelector('.profile__title')
 const profileJob = profileElement.querySelector('.profile__subtitle')
-
 const popupFormElement = popupElement.querySelector('.popup__form')
-const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button')
 
-
-const addTextProfile = function (evt) {
-    evt.preventDefault();
-    profileName.textContent = editName.value
-    profileJob.textContent = edinJob.value
-    closePopup()
-}
-
-
+// Функция открытия/закрытия попапа
 const openPopup = function () {
     popupElement.classList.add('popup_is-opened')
+    popupElementNameInput.value = profileName.textContent
+    popupElementJobInput.value = profileJob.textContent
 }
 const closePopup = function () {
     popupElement.classList.remove('popup_is-opened')
 }
 
+const formSubmitHandler = function (evt) {
+    evt.preventDefault();
+    profileName.textContent = profileName.value
+    profileJob.textContent = profileJob.value
+    closePopup()
+}
+
+// Регистрация обработчика событий по клику
 popupOpenButtonElement.addEventListener('click', openPopup)
 popupCloseButtonElement.addEventListener('click', closePopup)
-popupSubmitButtonElement.addEventListener('submit', addTextProfile);
+popupFormElement.addEventListener('submit', formSubmitHandler);
